52838ca52b27da43131fca7421089f7747bd32ef
hieumc <conghieu120@gmail.com>
2025-05-27 06:43:18 +0700

    update style

--- custom_theme/public/js/script.js
+++ custom_theme/public/js/script.js
@@ -2,6 +2,7 @@ $(function () {
     $('.sidebar-toggle-btn').off('click');
     $(".layout-side-section").removeClass("col-lg-2");
     $(".layout-main-section-wrapper").removeClass("col");
+    $(".page-body").removeClass("container");
 
     $('.sidebar-toggle-btn').on('click', function () {
         const $sideSection = $('.layout-side-section');
